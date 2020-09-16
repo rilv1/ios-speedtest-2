66a7da4db252db1a3cd59e4687a206f67f9ebb2e
فهد فهيد فهد - C53 <alsubaeih5@gmail.com>
2020-09-16 17:25:37 +0300

    تم

--- SPEED2.playground/Contents.swift
+++ SPEED2.playground/Contents.swift
@@ -9,10 +9,27 @@
 */
 var numbers = [1,2,3,4,5,6,7,8,9,10]
 
-
+for i in numbers {
+    
+    print(numbers)
+    
+}
 
 /*:
 
 # BONUS🎁 قم بكتابه كود يقوم بطباعة الأرقام الزوجية فقط 
 */
+var evenNumbers : [Int] = []
+
+for n in numbers {
+    
+    
+  
+    if n % 2 == 0 {
+        evenNumbers.append(n)
+    }
+    
+}
+
+print(evenNumbers)
 
